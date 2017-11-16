8e8448803e517b033359f4a1ed13d234e786793f
shikhadhankar <32158213+shikhadhankar@users.noreply.github.com>
2017-11-16 01:31:39 -0500

    Add files via upload

---
+++ cloud/shikha/lettercount/LetterCount.java
@@ -0,0 +1,109 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+
+import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.io.IntWritable;
+
+import org.apache.hadoop.io.LongWritable;
+
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class LetterCount {
+
+public static void main(String [] args) throws Exception
+
+{
+
+Configuration c=new Configuration();
+
+String[] files=new GenericOptionsParser(c,args).getRemainingArgs();
+
+Path input=new Path(files[0]);
+
+Path output=new Path(files[1]);
+
+Job j=new Job(c,"lettercount");
+
+j.setJarByClass(LetterCount.class);
+
+j.setMapperClass(MapForLetterCount.class);
+
+j.setReducerClass(ReduceForLetterCount.class);
+
+j.setOutputKeyClass(Text.class);
+
+j.setOutputValueClass(IntWritable.class);
+
+FileInputFormat.addInputPath(j, input);
+
+FileOutputFormat.setOutputPath(j, output);
+
+System.exit(j.waitForCompletion(true)?0:1);
+
+}
+
+public static class MapForLetterCount extends Mapper<LongWritable, Text, Text, IntWritable>{
+
+public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException
+
+{
+
+String line = value.toString();
+
+char[] carr=line.toCharArray();
+
+for(char c:carr )
+
+{
+
+      Text outputKey = new Text(String.valueOf(c));
+
+  IntWritable outputValue = new IntWritable(1);
+
+  con.write(outputKey, outputValue);
+
+}
+
+}
+
+}
+
+public static class ReduceForLetterCount extends Reducer<Text, IntWritable, Text, IntWritable>
+
+{
+
+public void reduce(Text word, Iterable<IntWritable> values, Context con) throws IOException, InterruptedException
+
+{
+
+int sum = 0;
+
+   for(IntWritable value : values)
+
+   {
+
+   sum += value.get();
+
+   }
+
+   con.write(word, new IntWritable(sum));
+
+}
+
+}
+
+}
