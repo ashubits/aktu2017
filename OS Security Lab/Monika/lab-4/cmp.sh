64e5a2e7313478f2b4f2e2c496312933e4fdd508
monikaahalawat <32158465+monikaahalawat@users.noreply.github.com>
2018-02-09 00:35:41 +0530

    Add files via upload
    program

---
+++ OS Security Lab/Monika/lab-4/cmp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+file="/home/monika/hash1.txt"
+while IFS=: read -r col1 col2
+do
+  echo "$col1" : $val2
+  echo "$col2" : $val1
+done < "$file"
+file="/home/monika/hash2.txt"
+while IFS=: read -r col3 col4
+do
+  echo "$col3" : $val4
+  echo "$col4" : $val3
+done < "$file"
+if ["$vol2" eq "$val4"];then
+  {
+   if ["$col1" != "$col3"];then
+    echo "Different files"
+    printf '%s\n' "$col3,$col4"
+    fi
+  }
+fi
