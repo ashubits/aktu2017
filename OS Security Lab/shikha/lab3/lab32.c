5468cf6a63fc7b7ce39c710dc69c6bc29fc5b09b
shikhadhankar <32158213+shikhadhankar@users.noreply.github.com>
2018-02-08 12:41:06 +0530

    Add files via upload

--- OS Security Lab/shikha/lab3/lab32.c
+++ OS Security Lab/shikha/lab3/lab32.c
@@ -1,4 +1,4 @@
-// thread
+// remove race condition using lock output is zero
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -10,11 +10,13 @@ pthread_mutex_t lock;
 
 void *poducerConsumer(void *quantity)
 {
-  pthread_mutex_lock(&lock);
+  
  int i,temp;
  int*q = (int*) quantity;
+
   for(i=0;i<5000000;i++)
    {
+pthread_mutex_lock(&lock);
      temp = available;
      temp = temp +*q;
      available = temp; 
@@ -36,5 +38,6 @@ pthread_create(&tid2,NULL,poducerConsumer,&y);
 
 pthread_join(tid1,NULL);
 pthread_join(tid2,NULL);
+pthread_mutex_destroy(&lock);
 printf("\n the availability of items are: %d",available); 
 }
